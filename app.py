e7e74a145435bb989e9adde71bec9f70a227acc8
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-12-10 17:10:49 +0800

    Add route for chopping text

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ cors = CORS(app, ressources={r"/api/*": {"origins" : ["http://localhost:*", "htt
 @app.route('/api/hello')
 def index():
   return jsonify({'hello': 'Hello, Flask!'})
+
+@app.route('/api/chop', methods=['POST'])
+def chop_text():
+  if not request.json or not 'text' in request.json:
+    abort(400)
+
+  chops = request.json['text'].split(" ")
+
+  return jsonify({'chops': chops})
 
 if __name__ == "__main__":
   app.run(debug=True)
