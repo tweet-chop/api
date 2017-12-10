d5fac8963e8e336b016504af9c6bb67aa589b98d
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-12-10 16:29:12 +0800

    Add CORS handling

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, ressources={r"/api/*": {"origins" : ["http://localhost:*", "https://tweet-chop.herokuapp.com*"]}})
 
-@app.route('/')
+@app.route('/api/hello')
 def index():
-  return 'Hello, Flask!11'
+  return jsonify({'hello': 'Hello, Flask!'})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   app.run(debug=True)
